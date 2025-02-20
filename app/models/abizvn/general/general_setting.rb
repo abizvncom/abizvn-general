676bd377884588ecdd4f3c8b946a0d286fe7e31f
huan <huanhvhd@gmail.com>
2025-02-20 13:10:07 +0700

    Adding version 0.1.2

--- app/models/abizvn/general/general_setting.rb
+++ app/models/abizvn/general/general_setting.rb
@@ -6,6 +6,10 @@ module Abizvn
       self.table_name = 'general_settings'
     
       validates :order, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+      validates :group, :code, :value, presence: true
+      validates :code, uniqueness: { scope: :group, case_insensitive: true, message: 'with code must be unique' }
+      validates :value, uniqueness: { scope: :group, case_insensitive: true, message: 'with value must be unique' }
     
       scope :ordered, -> { order(group: :asc, order: :asc) }
       scope :activated, -> { where(active: :true) }
